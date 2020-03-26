1e0accc5689537d58f9fa068e4a79c028a5a2d1c
awttt <350944364@qq.com>
2020-03-27 00:21:35 +0800

    第一次提交

---
+++ src/dom.js
@@ -0,0 +1,55 @@
+window.dom = {
+  create(string) {
+    const container = document.createElement("template");
+    container.innerHTML = string.trim();
+    return container.content.firstChild;
+  },
+  after(node, node2) {
+    node.parentNode.insertBefore(node2, node.nextSibling);
+  },
+  before(node, node2) {
+    node.parentNode.insertBefore(node2, node);
+  },
+  append(parent, node) {
+    parent.appendChild(node);
+  },
+  wrap(node, parent) {
+    dom.before(node, parent);
+    dom.append(parent.node);
+  },
+  attr(node, title, value) {
+    if (arguments.length === 3) {
+      node.setAttribute(name, value);
+    } else if (arguments.length === 2) {
+      return node.getAttribute(name);
+    }
+  },
+  text(node, text) {
+    node.innerText = string;
+  },
+  find(selector, scope) {
+    return (scope || document).querySelectorAll(selector);
+  },
+  style(node, name, value) {
+    if (arguments.length === 3) {
+      // dom.style(div, 'color', 'red')
+      node.style[name] = value;
+    } else if (arguments.length === 2) {
+      if (typeof name === "string") {
+        // dom.style(div, 'color')
+        return node.style[name];
+      } else if (name instanceof Object) {
+        // dom.style(div, {color: 'red'})
+        const object = name;
+        for (let key in object) {
+          node.style[key] = object[key];
+        }
+      }
+    }
+  },
+  each(nodeList, fn) {
+    for (let i = 0; i < nodeList.length; i++) {
+      fn.call(null, nodeList[i]);
+    }
+  }
+};
